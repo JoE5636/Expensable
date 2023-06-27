708081b53c9bd203e9c841a251f1e46764c2198a
JoE5636 <joeldelarosalopez@gmail.com>
2023-06-27 14:48:09 -0600

    loginpage advanced

--- src/UnauthenticatedApp.js
+++ src/UnauthenticatedApp.js
@@ -1,15 +1,28 @@
 import { Route, Routes, Navigate } from "react-router-dom";
+import styled from "@emotion/styled";
 import LoginPage from "./pages/login-page";
+import { colors } from "./styles";
 
+const Container = styled.div`
+  margin-top: 140px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  background-color: ${colors.white};
+`;
 export default function UnauthenticatedApp() {
   return (
-    <Routes>
-      <Route path="/">
-        <Route index element={<Navigate to="/login" replace={true} />} />
-        <Route path="login" element={<LoginPage />} />
-        <Route path="signup" element={<p> Sign up</p>} />
-        <Route path="*" element={<Navigate to="/login" replace={true} />} />
-      </Route>
-    </Routes>
+    <Container>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Navigate to="/login" replace={true} />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="signup" element={<p> Sign up</p>} />
+          <Route path="*" element={<Navigate to="/login" replace={true} />} />
+        </Route>
+      </Routes>
+    </Container>
   );
 }
--- src/pages/login-page.js
+++ src/pages/login-page.js
@@ -1,5 +1,58 @@
 import React from "react";
+import styled from "@emotion/styled";
+import { typography, colors } from "../styles";
 import { useAuth } from "../context/auth-context";
+
+const Title = styled.h1`
+  ${typography.head.lg}
+  font-weight: 600;
+`;
+
+const Form = styled.form`
+  width: 100%;
+  border-radius: 6px;
+  color: ${colors.gray[600]};
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 10px;
+  background-color: ${colors.white};
+`;
+
+const Label = styled.label`
+  font-size: 12px;
+  font-family: Inter;
+  line-height: 16px;
+  letter-spacing: 1.5px;
+  text-transform: uppercase;
+`;
+
+const Input = styled.input`
+  box-shadow: 0px 1px 2px 0px rgba(0, 0, 0, 0.05);
+  padding: 8px 12px;
+  width: 100%;
+  border-radius: 6px;
+  border: 1px solid ${colors.gray[200]};
+  background: ${colors.white};
+`;
+
+const SubmitButton = styled.button`
+  display: flex;
+  justify-content: center;
+  padding: 6px 16px;
+  align-items: flex-start;
+  color: ${colors.white};
+  border: none;
+  border-radius: 6px;
+  cursor: pointer;
+  width: 100%;
+  background-color: ${colors.pink[600]};
+  ${typography.text.md};
+  &:active {
+    background-color: ${colors.pink[400]};
+  }
+`;
 
 export default function LoginPage() {
   const { login } = useAuth();
@@ -18,19 +71,36 @@ export default function LoginPage() {
   }
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input id="email" name="email" />
+    <div
+      style={{
+        width: "400px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "10px",
+      }}
+    >
+      <Title>Login</Title>
+      <Form onSubmit={handleSubmit}>
+        <div
+          style={{
+            width: "100%",
+          }}
+        >
+          <Label htmlFor="email">Email</Label>
+          <Input id="email" name="email" />
         </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input id="password" name="password" />
+        <div
+          style={{
+            width: "100%",
+          }}
+        >
+          <Label htmlFor="password">Password</Label>
+          <Input id="password" name="password" />
         </div>
-        <button type="submit">Login</button>
-      </form>
+        <SubmitButton type="submit">Login</SubmitButton>
+      </Form>
     </div>
   );
 }
